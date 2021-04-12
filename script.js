e02aacb112d0f4a973eb03e055890f4ac5a2e2df
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-12 16:34:08 +0400

    Learn 39. Done.

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
 'use strict';
 
+// const timerId = setTimeout(function(text) {
+//     console.log(text);
+// }, 2000, 'hello!');
+
+const btn = document.querySelector('.btn');
+// let timerId, i = 0;
+
+function myAnim() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        };
+        pos++;
+        elem.style.top = pos + 'px';
+        elem.style.left = pos + 'px';
+    }
+}
+
+btn.addEventListener('click', myAnim);
+
+// btn.addEventListener('click', () => {
+//     // const timerId = setTimeout(logger, 2000);
+//     timerId = setInterval(logger, 1000);
+// });
+
+// const timerId = setTimeout(logger, 2000);
+
+// function logger() {
+//     if (i > 1) {
+//         clearInterval(timerId);
+//     }
+//     console.log(`text${i}`);
+//     i++;
+// };
+
+// clearInterval(timerId);
+
+// let id = setTimeout(function log() {
+//     console.log('Hello!');
+//     id = setTimeout(log, 500);
+// }, 500);
