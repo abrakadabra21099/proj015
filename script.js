3932cdbde35bb2e302d5d6238d8c82902f6e5ebc
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-12 16:40:04 +0400

    Learn 40. Init.

--- script.js
+++ script.js
@@ -1,47 +1,5 @@
 'use strict';
 
-// const timerId = setTimeout(function(text) {
-//     console.log(text);
-// }, 2000, 'hello!');
+const now = new Date();
 
-const btn = document.querySelector('.btn');
-// let timerId, i = 0;
-
-function myAnim() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
-
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        };
-        pos++;
-        elem.style.top = pos + 'px';
-        elem.style.left = pos + 'px';
-    }
-}
-
-btn.addEventListener('click', myAnim);
-
-// btn.addEventListener('click', () => {
-//     // const timerId = setTimeout(logger, 2000);
-//     timerId = setInterval(logger, 1000);
-// });
-
-// const timerId = setTimeout(logger, 2000);
-
-// function logger() {
-//     if (i > 1) {
-//         clearInterval(timerId);
-//     }
-//     console.log(`text${i}`);
-//     i++;
-// };
-
-// clearInterval(timerId);
-
-// let id = setTimeout(function log() {
-//     console.log('Hello!');
-//     id = setTimeout(log, 500);
-// }, 500);
+console.log(now);
