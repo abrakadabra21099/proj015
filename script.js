d375fff3cf54240c1d5340a51b93893dd617334f
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-14 16:53:04 +0400

    Learn 47. Done.

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 'use strict';
 
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+
+    calcArea() {
+        return this.w * this.h;
+    }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(w, h, text, bgColor) {
+        super(w, h);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, Цвет: ${this.bgColor}`);
+    }
+}
+
+const square = new Rectangle(10, 10);
+const loSquare = new Rectangle(20, 100);
+console.log(square.calcArea());
+console.log(loSquare.calcArea());
+
+const cRect = new ColoredRectangleWithText(8, 12, 'abc', 'red');
+cRect.showMyProps();
+console.log(cRect.calcArea());
